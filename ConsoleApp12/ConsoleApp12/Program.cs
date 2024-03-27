c666d5f95e343d36b912eb9711ffdaba2aa78889
FallenAngel-coder <76203302+FallenAngel-coder@users.noreply.github.com>
2024-03-27 19:54:07 +0200

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskEx1();
+            TaskEx2();
+            Console.WriteLine("Продовження коду");
+            Console.ReadKey();
+            Console.WriteLine("-----------------------------------");
+            NoTask1();
+            NoTask2();
+            Console.ReadKey();
+        }
+        public static async Task TaskEx1()
+        {
+            Task task1 = Task.Run(() =>
+            {
+                Console.WriteLine("Початок виконання завдання 1...");
+                Task.Delay(4000).Wait(); 
+                Console.WriteLine("Завершення виконання завдання 1.");
+            });
+            await task1; 
+        }
+        public static async Task TaskEx2()
+        {
+            Task task2 = Task.Run(() =>
+            {
+                Console.WriteLine("Початок виконання завдання 2...");
+                Task.Delay(3000).Wait();
+                Console.WriteLine("Завершення виконання завдання 2.");
+            });
+            await task2.ContinueWith((t) =>
+            {
+                Console.WriteLine("Код після виконання завдання 2.");
+            });
+        }
+        public static void NoTask1()
+        {
+            Console.WriteLine("Початок виконання завдання 1...");
+            Thread.Sleep(4000);
+            Console.WriteLine("Завершення виконання завдання 1.");
+        }        
+        public static void NoTask2()
+        {
+            Console.WriteLine("Початок виконання завдання 2...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Завершення виконання завдання 2.");
+        }
+    }
+}
---
+++ ConsoleApp12/Exam2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace Exam2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Enumerable.Range(1, 100000000).ToArray();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            double[] results = new double[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //results[i] = Math.Log(numbers[i]);
+                Console.WriteLine("AAA");
+                Console.Clear();
+            }
+            long first = stopwatch.ElapsedMilliseconds;
+            stopwatch.Stop();
+            stopwatch.Reset();
+            stopwatch.Start();
+            double[] parallelResults = new double[numbers.Length];
+            Parallel.For(0, numbers.Length, i =>
+            {
+                //parallelResults[i] = Math.Log(numbers[i]);
+                Console.WriteLine("AAA");
+                Console.Clear();
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"Час виконання звичайного циклу: {first} мс");
+            Console.WriteLine($"Час виконання з Parallel: {stopwatch.ElapsedMilliseconds} мс");
+            bool resultsMatch = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (results[i] != parallelResults[i])
+                {
+                    resultsMatch = false;
+                    break;
+                }
+            }
+            if (resultsMatch)
+                Console.WriteLine("Результати співпадають.");
+            else
+                Console.WriteLine("Результати не співпадають.");
+            Console.ReadKey();
+        }
+    }
+}
